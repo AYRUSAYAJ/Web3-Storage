964c87be0bb0a332c993b5748320fd62eed49cd7
AYRUSAYAJ <notifi1jaya@gmail.com>
2023-11-12 15:35:00 +0530

    Add redirect to dashboard after successful file deletion

--- index.cjs
+++ index.cjs
@@ -8,7 +8,7 @@ const upload = multer({ dest: 'uploads/' });
 app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/metamask.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/dashboard', (req, res) => {
