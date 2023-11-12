083fbee8bd8ff86bccd2c66b86d991fee8642399
AYRUSAYAJ <notifi1jaya@gmail.com>
2023-11-12 18:29:29 +0530

    Add file upload functionality and recieved in dashboard

--- index.cjs
+++ index.cjs
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const app = express();
+const uploadedFiles = [];
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -24,33 +25,34 @@ app.post('/upload', upload.array('files[]'), async (req, res) => {
   const { createHelia } = await import('helia');
   const { unixfs } = await import('@helia/unixfs');
   const helia = await createHelia();
-  const uploadedFiles = [];
 
   const files = req.files;
 
   if (files && files.length > 0) {
-    
-
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
       const fileName = file.originalname;
       const fileBuffer = fs.readFileSync(file.path);
       const fsUnix = unixfs(helia);
-      const cid = await fsUnix.addBytes(fileBuffer,helia.blockstore);
+      const cid = await fsUnix.addBytes(fileBuffer, helia.blockstore);
       uploadedFiles.push({ fileName, cid });
       fs.unlinkSync(file.path);
     }
   }
-  res.json(JSON.stringify(uploadedFiles));
-  console.log(uploadedFiles);
 
+  // Send a response indicating that the upload is complete
+  res.redirect('/dashboard');
+});
 
+app.get('/uploadedFiles', (req, res) => {
+  // Respond with the updated uploadedFiles array
+  res.json(uploadedFiles);
 });
 
 app.get('/delete', (req, res) => {
   res.sendFile(__dirname + '/public/delete.html');
 });
 
-app.listen(4200, () => {
-  console.log('Server started on port 4200');
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
