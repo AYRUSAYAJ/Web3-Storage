ae0803fadc07d17f38667c7db99bc3a4b939a177
AYRUSAYAJ <notifi1jaya@gmail.com>
2023-11-14 21:09:43 +0530

    Improve Metamask login UI

--- index.cjs
+++ index.cjs
@@ -52,6 +52,6 @@ app.get('/delete', (req, res) => {
   res.sendFile(__dirname + '/public/delete.html');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(4200, () => {
+  console.log('Server started on port 4200');
 });
